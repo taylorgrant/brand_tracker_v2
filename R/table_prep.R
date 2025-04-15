982f0002aad7e3b020d7da295df9a0c17e7536fa
taylorgrant <taylorgrant38@gmail.com>
2025-04-14 19:17:39 -0700

    prep data for tables or graphs

---
+++ R/table_prep.R
@@ -0,0 +1,29 @@
+#' Prep for GT table 
+#' 
+#' Need to get the crosstab groups that are going to stay in the data table. 
+#'
+#' @param dat Single dataframe of results from the list
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' filters <- table_prep(result_list[[1]])
+#' }
+table_prep <- function(dat) {
+  if (any(stringr::str_detect(colnames(dat) ,"group_2"))) { # 2 groups could be a demo and campaign source
+    grp2 <- select.list(title = "\nWhich groups would you like to keep in the table?",
+                        choices = unique(dat$group_2), multiple = TRUE)
+    grp1 <- select.list(title = "\nWhich groups would you like to keep in the table?",
+                        choices = unique(dat$group_1), multiple = TRUE)
+    group_filter <- c(grp1, grp2)
+  } else if (!any(stringr::str_detect(colnames(dat), "group_2")) & any(stringr::str_detect(colnames(dat), "group_1"))) {
+    grp1 <- select.list(title = "\nWhich groups would you like to keep in the table?",
+                        c(unique(dat$group_1)), multiple = TRUE)
+    group_filter <- c(grp1, grp2 = NULL)
+  } else {
+    group_filter <- c(grp1 = NULL, grp2 = NULL)
+  }
+  return(group_filter)
+}
