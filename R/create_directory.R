9e9b8c96e7ee70e17add19d1ac304e842fe00b39
taylorgrant <taylorgrant38@gmail.com>
2025-04-14 19:15:46 -0700

    create a directory for dumbbell plots

---
+++ R/create_directory.R
@@ -0,0 +1,30 @@
+create_directory <- function(brand, filters, main_subfolders = c("tables", "figures", "competitive", "raw_data"), subfolders = c("Campaign", "Social", "Digital", "TV")) {
+
+    main_path <- here::here("processed",glue::glue("{brand}-{stringr::str_replace(filters, '/', '-')}_{Sys.Date()}"))
+    if (!dir.exists(main_path)) {
+      dir.create(main_path, recursive = TRUE)
+    } 
+  # }
+    
+  # Create the main subfolders ("tables", "figures", and "competitive")
+  purrr::walk(main_subfolders, function(main_subfolder) {
+    main_subfolder_path <- file.path(main_path, main_subfolder)
+    if (!dir.exists(main_subfolder_path)) {
+      dir.create(main_subfolder_path)
+    } 
+    
+    # Only create subfolders for "tables" and "figures"
+    if (main_subfolder %in% c("tables", "figures")) {
+      purrr::walk(subfolders, function(subfolder) {
+        subfolder_path <- file.path(main_subfolder_path, subfolder)
+        if (!dir.exists(subfolder_path)) {
+          dir.create(subfolder_path)
+        }
+      })
+    }
+  })
+  return(main_path)
+  }
+
+
+
